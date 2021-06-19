57e4ffc30869daf956283ccb4375b4a64cbc7c6e
NicolasGDas <nico.g.das@gmail.com>
2021-06-19 11:08:08 -0300

    Agregado de escondido de teclado si apretas fuera del campo

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback,Keyboard} from 'react-native';
 import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import ReviewForm from './reviewForm';
 
+// const ReviewSchema = yup.object({
+//     title:
+// })
 
 export default function Home({navigation}) {
 
@@ -28,16 +31,19 @@ export default function Home({navigation}) {
             <View style={globalStyles.container} >
 
                 <Modal visible={modalOpen} animationType='slide'>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
                     <View style={styles.modalContent}>
                     <MaterialIcons
                     name= 'close'
                     size={24}
                     style={{...styles.modalToggle,...styles.modalClose}}
                     onPress={()=> setModalOpen(false)}
-                />
+                    />
                         <ReviewForm addReview={addReview}/>
                     </View>
 
+                    </TouchableWithoutFeedback>
                 </Modal>
 
                 <MaterialIcons
