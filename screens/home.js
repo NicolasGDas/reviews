6694dd67e72ae6bede47f217b55cfef3ff1179e9
NicolasGDas <nico.g.das@gmail.com>
2021-06-19 17:56:14 -0300

    Update de home, agregado de modal para editar,eliminar

--- screens/home.js
+++ screens/home.js
@@ -1,36 +1,90 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback,Keyboard} from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, 
+    Alert ,TouchableWithoutFeedback,Keyboard} from 'react-native';
 import Card from '../shared/card';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
-import ReviewForm from './reviewForm';
+import ReviewFormAdd from './reviewFormAdd';
+import ReviewFormEdit from './reviewFormEdit';
+import DeleteReview from './deleteReview';
+import { StatusBar } from 'expo-status-bar';
+import {AppLoading} from 'expo'
 
-// const ReviewSchema = yup.object({
-//     title:
-// })
+
+
 
 export default function Home({navigation}) {
-
-    const [modalOpen,setModalOpen] = useState(false);
+    {/*STATES--------------------------------------------------------- */}
+    const [selectedItem,setSelectedItem]=useState()
+    const [modalDeleteOpen,setModalDeleteOpen]=useState(false)
+    const [modalAddOpen,setModalAddOpen] = useState(false);
+    const [modalEditOpen,setModalEditOpen] = useState(false);
     const [reviews, setReviews] = useState([
         {title: 'Bloodborne',rating: 5, body: 'lorem ipsum',key:'1'},
         {title: 'Witcher 3',rating: 5, body: 'El mejor juego, no me dan los corazones para el rating',key:'2'},
         {title: 'World of Warcraft',rating: 2, body: 'lorem ipsum',key:'3'},
         {title: 'League of Legends',rating: 1, body: 'lorem ipsum',key:'4'}
     ]);
+    {/**--------------------------------------------------------------- */}
 
+    {/**Funtions and variables ------------------------------------------------------------------ */}
     const addReview = (review)=>{
         review.key = Math.random().toString();
         setReviews((currentReviews)=>{
             return[review,...currentReviews]
         });
-        setModalOpen(false)
+        setModalAddOpen(false)
 
     }
+    const editReview = (reviewToUpdate)=>{
+        let i =0
+        let encontrado = false
+        while (i < reviews.length && !encontrado){
+            if(reviews[i].key === reviewToUpdate.id){
+                reviews[i].title = reviewToUpdate.title
+                reviews[i].body = reviewToUpdate.body
+                reviews[i].rating = reviewToUpdate.rating
+                encontrado = true
+            }
+            i = i +1
+        }
+        setModalEditOpen(false)
+
+    }
+    
+    const handleSelectedItem = (id)=>{
+        setModalEditOpen(true)
+        const selected = reviews.filter((review =>{ if(review.key === id){return review}}))
+        setSelectedItem(selected[0])
+    }
+    const handleSelectedItemDelete = (id)=>{
+        setModalDeleteOpen(true)
+        const selected = reviews.filter((review =>{ if(review.key === id){return review}}))
+        setSelectedItem(selected[0])
+
+        
+    }
+    const deleteReview =  (values)=>{
+        console.log("Entre al delete")
+        reviews.splice((review =>{ if(review.key === values.id){return review}}),1)
+        console.log(reviews)
+        setModalDeleteOpen(false)
+        
+    }
+
+    const dataModify = {editReview,selectedItem}
+    const dataDelete = {deleteReview,selectedItem}
+    {/**Funtions ------------------------------------------------------------------ */}
+
+
+
         return (
             <View style={globalStyles.container} >
-
-                <Modal visible={modalOpen} animationType='slide'>
+                <StatusBar
+                    hidden
+                />
+                {/**MODAL E AGREGADO------------- */}
+                <Modal visible={modalAddOpen} animationType='slide'>
                     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 
                     <View style={styles.modalContent}>
@@ -38,31 +92,80 @@ export default function Home({navigation}) {
                     name= 'close'
                     size={24}
                     style={{...styles.modalToggle,...styles.modalClose}}
-                    onPress={()=> setModalOpen(false)}
+                    onPress={()=> setModalAddOpen(false)}
                     />
-                        <ReviewForm addReview={addReview}/>
+                        <ReviewFormAdd addReview={addReview}/>
                     </View>
 
                     </TouchableWithoutFeedback>
                 </Modal>
+                {/**MODAL DE EDITION */}
+                <Modal visible={modalEditOpen} animationType='slide'>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+                    <View style={styles.modalContent}>
+                    <MaterialIcons
+                    name= 'close'
+                    size={24}
+                    style={{...styles.modalToggle,...styles.modalClose}}
+                    onPress={()=> setModalEditOpen(false)}
+                    />
+                        <ReviewFormEdit param={dataModify}/>
+                    </View>
+
+                    </TouchableWithoutFeedback>
+                </Modal>      
+                {/**MODAL DE Delete */}
+                <Modal visible={modalDeleteOpen} animationType='slide'>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+                    <View style={styles.modalContent}>
+                    <MaterialIcons
+                    name= 'close'
+                    size={24}
+                    style={{...styles.modalToggle,...styles.modalClose}}
+                    onPress={()=> setModalDeleteOpen(false)}
+                    />
+                        <DeleteReview params={dataDelete}/>
+                    </View>
+
+                    </TouchableWithoutFeedback>
+                </Modal>      
 
                 <MaterialIcons
                     name= 'add'
                     size={24}
                     style={styles.modalToggle}
-                    onPress={()=> setModalOpen(true)}
+                    onPress={()=> setModalAddOpen(true)}
                 />
-
+                {/**First modal for adding------------- */}
+                {/**Flat list items listed--------------------------- */}
                 <FlatList 
                     data={reviews}
                     renderItem = {({ item }) => (
-                        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)} >
                             <Card>
                                 <Text style={globalStyles.titleText}>{ item.title }</Text>
+                                <MaterialIcons
+                                    name= 'edit'
+                                    size={24}
+                                    style={styles.editbtn}
+                                    onPress={()=> handleSelectedItem(item.key)}
+                                />
+                                <MaterialIcons
+                                    name= 'delete'
+                                    size={24}
+                                    style={styles.deletebtn}
+                                    onPress={()=> handleSelectedItemDelete(item.key)}
+                                />
                             </Card>
                         </TouchableOpacity>
                     )}
                 />
+                {/**Flat list items listed--------------------------- */}
+                
+
+
             </View>
         )
 
@@ -86,5 +189,25 @@ const styles = StyleSheet.create({
     },
     modalContent: {
         flex: 1,
-    }
+    },
+    deletebtn:{
+        position:'absolute',
+        right: 0,
+        flexDirection:'row',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+    },
+    editbtn:{
+        position:'absolute',
+        right: 45,
+        flexDirection:'row',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+    },
 });
