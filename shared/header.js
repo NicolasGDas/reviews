da2f0077aee0305a7cbc91170f2eadabd7c1e01d
NicolasGDas <nico.g.das@gmail.com>
2021-06-18 21:24:16 -0300

    Arreglado de style del header

--- shared/header.js
+++ shared/header.js
@@ -10,19 +10,19 @@ export default function Header({ title, navigation }) {
 
     return (
         <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
-        <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-        <View style={styles.headerTitle}>
-            <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
-            <Text style={styles.headerText}>{title}</Text>
-        </View>
+            <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
+            <View style={styles.headerTitle}>
+                <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
+                <Text style={styles.headerText}>{title}</Text>
+            </View>
         </ImageBackground>
     );
     }
 
     const styles = StyleSheet.create({
     header: {
-        width: '100%',
-        height: '100%',
+        width: '110%',
+        height: '110%',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
