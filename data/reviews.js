66670bd42e1ca8d910e0f364610257eb0d782e24
NicolasGDas <nico.g.das@gmail.com>
2021-06-19 23:51:07 -0300

    Creacion de plugin para almacen de revies fijas

---
+++ data/reviews.js
@@ -0,0 +1,6 @@
+export const data = [
+    {title: 'Bloodborne',rating: 5, body: 'lorem ipsum',key:'1'},
+    {title: 'Witcher 3',rating: 5, body: 'El mejor juego, no me dan los corazones para el rating',key:'2'},
+    {title: 'World of Warcraft',rating: 2, body: 'lorem ipsum',key:'3'},
+    {title: 'League of Legends',rating: 1, body: 'lorem ipsum',key:'4'}
+]
