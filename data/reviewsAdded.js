125ac66ae9151e27bd1b053bc05a65d72673a8c7
NicolasGDas <nico.g.das@gmail.com>
2021-06-21 01:30:29 -0300

    Cambios en las datas de las agregadas

--- data/reviewsAdded.js
+++ data/reviewsAdded.js
@@ -1,7 +1,13 @@
-let Reviews = []
+export let Reviews = []
 
 export function addReviewTemp(review){
     Reviews.push(review)
     console.log("ADDED")
     console.log(Reviews)
+}
+
+
+
+export function getAddedReviews(){
+    return Reviews
 }
