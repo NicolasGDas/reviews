7a876981de60b26efd3b05eb0576cbbdfc77be39
NicolasGDas <nico.g.das@gmail.com>
2021-06-21 01:27:00 -0300

    Jugos hardcodeados

---
+++ data/juegos.js
@@ -0,0 +1,6 @@
+export const juegos =[
+    {title: "Witcher 3", key: "1",image:require('../assets/witcher3.png')},
+    {title: "Bloodborne", key: "13", image:require('../assets/bloodborn.png')},
+    {title: "Battlefield 1", key: "14", image:require('../assets/Bf1.jpg')},
+
+]
