544ff02cb308733454053b47efdb6267bbdcf4d5
NicolasGDas <nico.g.das@gmail.com>
2021-06-21 01:28:49 -0300

    Reviewers hardcodeados

---
+++ data/reviewers.js
@@ -0,0 +1,6 @@
+export const reviewersDb = [
+    {nombre_rev: "Lesli", key: "1", user_name: "@algo", image:require('../assets/girl.png')},
+    {nombre_rev: "Geralt", key: "2", user_name: "@algo", image:require('../assets/geralt.jpg')},
+    {nombre_rev: "Viking", key: "3", user_name: "@algo", image:require('../assets/viking.png')},
+    
+]
